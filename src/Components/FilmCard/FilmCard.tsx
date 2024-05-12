042f24bb818adbe1b66c17f643857d20a18d4c9a
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2024-05-12 19:12:52 +0300

    made filmcard inline

--- src/Components/FilmCard/FilmCard.tsx
+++ src/Components/FilmCard/FilmCard.tsx
@@ -1,22 +1,18 @@
 import { Card, CardMedia } from "@mui/material";
-import { Movie } from "../../Pages/Homepage/Homepage";
+import { Movie } from "../../util/interface";
 
-function FilmCard({ apiResponse }: { apiResponse: Movie[] }) {
-  if (!apiResponse || apiResponse.length === 0) {
-    return <p>No movies found</p>;
-  }
-
+function FilmCard({ apiResponse }: { apiResponse: Movie }) {
   return (
     <>
-      <>
-        <Card sx={{ maxWidth: 345 }}>
+      <div style={{ display: "inline-block", margin: "4px" }}>
+        <Card sx={{ width: "100%" }}>
           <CardMedia
-            sx={{ height: 400 }}
-            image={`https://image.tmdb.org/t/p/w300/${apiResponse[1].poster_path}`}
+            sx={{ height: 450, width: 300 }}
+            image={`https://image.tmdb.org/t/p/w300/${apiResponse.poster_path}`}
             title="green iguana"
           />
         </Card>
-      </>
+      </div>
     </>
   );
 }
