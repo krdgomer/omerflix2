fea4c635ce859f9efbd12482cbf58a4cee60a8c8
Ömer Can Karadağ <krdg.omercan@hotmail.com>
2024-05-12 19:13:08 +0300

    added horizontal scrolling menu

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,42 +1,51 @@
 import "./homepage.css";
 import Header from "../../Components/Header/Header";
 import FilmCard from "../../Components/FilmCard/FilmCard";
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-export interface Movie {
-  poster_path: string;
-  // Add other properties if needed
-}
+import { useEffect, useRef, useState } from "react";
+import { Movie } from "../../util/interface";
+import { getTrendingMovieData } from "../../util/utils";
+import { Grid, Slider } from "@mui/material";
 
 function Homepage() {
   const [movieData, setMovieData] = useState<Movie[]>([]);
   const [loading, setLoading] = useState<boolean>(true); // Add loading state
+  const scrollTimeoutRef = useRef<number | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    async function getTrendingMovieData(type: string) {
-      try {
-        const apiKey = "a5c19fd857457dfe953dfcbd352d50fe";
-        let resp = await axios.get(
-          `https://api.themoviedb.org/3/trending/${type}/day?api_key=${apiKey}&media_type=movie`
-        );
-        console.log(21, resp.data.results);
-
-        setMovieData(resp.data.results);
-        setLoading(false); // Update loading state
-      } catch (error) {
-        console.error("Error fetching movie data:", error);
-        setLoading(false); // Ensure loading state is updated even in case of an error
-      }
-    }
-
-    getTrendingMovieData("movie");
+    getTrendingMovieData("movie", setMovieData, setLoading);
   }, []); // Empty dependency array ensures this effect runs only once when the component mounts
+
+  const handleWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += event.deltaY;
+      event.preventDefault();
+
+      // Debounce scroll events
+      if (scrollTimeoutRef.current) {
+        clearTimeout(scrollTimeoutRef.current);
+      }
+
+      scrollTimeoutRef.current = setTimeout(() => {
+        scrollTimeoutRef.current = null;
+      }, 10); // Adjust debounce delay as needed
+    }
+  };
 
   return (
     <>
       <Header />
-      {loading ? <p>Loading...</p> : <FilmCard apiResponse={movieData} />}
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="trending" ref={containerRef} onWheel={handleWheel}>
+            {movieData.map((movie, index) => (
+              <FilmCard key={index} apiResponse={movie} />
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 }
